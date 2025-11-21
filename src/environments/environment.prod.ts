d60b367dff4005e196c567d0d51059e9a1493dbb
ledger-manager <sreedher459@gmail.com>
2025-11-21 18:54:19 -0500

    db name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   BASE_URL: 'https://holy-calorifacient-rosendo.ngrok-free.dev/api',
-  DB_NAME: 'mc_manager_prod'
+  DB_NAME: 'mcm_prod'
   // No API_USER_NAME or API_PASSWORD in production
 };
